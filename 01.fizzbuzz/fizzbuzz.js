7d2e12d2bb4370acaad80e67a688cbffafaebc6a
kekosh <kenta0540@gmail.com>
2022-12-17 17:12:53 +0900

    [レビュー指摘事項修正] レビューコメントの内容を反映

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,20 +1,20 @@
+const FIZZ = "Fizz";
+const BUZZ = "Buzz";
+
 for (let num = 1; num <= 20; num++){
-    console.log(print_msg(num))
+    console.log(print_msg(num));
 }
 
 function print_msg(num) {
-    const FIZZ = "Fizz";
-    const BUZZ = "Buzz";
-
-    if (num % 3 == 0 && num % 5 == 0) {
+    if (num % 3 === 0 && num % 5 === 0) {
         return FIZZ + BUZZ;
     }
 
-    if (num % 3 == 0) {
+    if (num % 3 === 0) {
         return FIZZ;
     }
 
-    if (num % 5 == 0) {
+    if (num % 5 === 0) {
         return BUZZ;
     }
 
