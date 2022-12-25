e50dfe054f86a55057460db75ef5e123d4c0dea1
kekosh <kenta0540@gmail.com>
2022-12-25 13:30:22 +0900

    固定月のカレンダー出力処理実装完了

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -20,18 +20,21 @@ let first_day_of_the_month = getDay(new Date(year, month - 1, 1));
 
 // 月末日
 let last_day_of_month = lastDayOfMonth(dateInstance).getDate();
+let format_week = ["  ", "  ", "  ", "  ", "  ", "  ", "  "];
+let day_index = first_day_of_the_month;
+let current_date = 1;
 
-// let row_week = ["", "", "", "", "", "", ""];
-// let row_week_index = first_day_of_the_month;
-// for (let i = 1; i <= last_day_of_month; i++) {
-//   row_week[row_week_index] = i;
-//   if (row_week_index == 6) {
-//     console.log(row_week);
-//     row_week_index = 0;
-//   } else {
-//     row_week_index += 1;
-//   }
-// }
+for (var i = 0; i < last_day_of_month; i++) {
+  format_week[day_index] =
+    String(current_date).length == 1 ? ` ${current_date}` : `${current_date}`;
+  current_date++;
+  day_index++;
+  if (day_index > 6) {
+    console.log(format_week.join(" "));
+    format_week = ["  ", "  ", "  ", "  ", "  ", "  ", "  "];
+    day_index = 0;
+  }
+}
 
 // 月末日の曜日
 // let day_of_the_month = getDay(new Date(year, month - 1, last_day_of_month));
