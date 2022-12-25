33fe38ec79c82573840b91177528f101d95e2653
kekosh <kenta0540@gmail.com>
2022-12-25 12:42:59 +0900

    曜日ヘッダーの作成を独自関数からjoinによる作成に変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,3 +1,6 @@
+var lastDayOfMonth = require("date-fns/lastDayOfMonth");
+var getDay = require("date-fns/getDay");
+
 const DAY_OF_WEEK = ["日", "月", "火", "水", "木", "金", "土"];
 
 let dateInstance = new Date();
@@ -7,15 +10,29 @@ let date = dateInstance.getDate();
 let day = dateInstance.getDay();
 
 let header = `      ${month}月 ${year}     `;
+
+//ヘッダー出力
 console.log(header);
-console.log(header_of_the_day(DAY_OF_WEEK));
-// console.log(`${year}/${month}/${date}(${DAY_OF_WEEK[day]})`);
+console.log(DAY_OF_WEEK.join(" "));
 
-function header_of_the_day(days) {
-  let header_str = "";
-  days.forEach((day) => {
-    header_str += day + " ";
-  });
+// 月初の曜日
+let first_day_of_the_month = getDay(new Date(year, month - 1, 1));
 
-  return header_str.trimEnd();
-}
+// 月末日
+let last_day_of_month = lastDayOfMonth(dateInstance).getDate();
+
+// let row_week = ["", "", "", "", "", "", ""];
+// let row_week_index = first_day_of_the_month;
+// for (let i = 1; i <= last_day_of_month; i++) {
+//   row_week[row_week_index] = i;
+//   if (row_week_index == 6) {
+//     console.log(row_week);
+//     row_week_index = 0;
+//   } else {
+//     row_week_index += 1;
+//   }
+// }
+
+// 月末日の曜日
+// let day_of_the_month = getDay(new Date(year, month - 1, last_day_of_month));
+// console.log(DAY_OF_WEEK[day_of_the_month]);
